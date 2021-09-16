1e77fb1ae7396a92bdae568b2f0eef8066e3c47c
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-17 00:51:32 +0800

    adjusted spinner loading time

--- assets/application.js
+++ assets/application.js
@@ -1,10 +1,20 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+// window.addEventListener('load', function() {
+//     const loader = document.getElementById('loading');
+//     setTimeout(function() {
+//         loader.style.opacity = '0';
+//         setTimeout(function() {
+//             loader.remove();
+//         }, 500)
+//     }, 1300);
+// })
+
+window.addEventListener("load", function(event) {
     const loader = document.getElementById('loading');
     setTimeout(function() {
         loader.style.opacity = '0';
         setTimeout(function() {
             loader.remove();
         }, 500)
-    }, 1300);
-})
+    }, 900);
+  });
