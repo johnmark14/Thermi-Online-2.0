43f4c36ea9eed247d068ba0d1147c8534629ea10
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-17 00:44:39 +0800

    added loading spinner

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,10 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    const loader = document.getElementById('loading');
+    setTimeout(function() {
+        loader.style.opacity = '0';
+        setTimeout(function() {
+            loader.remove();
+        }, 500)
+    }, 1300);
+})
