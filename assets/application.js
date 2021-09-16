2b70187327ee678bd6900a77121bbd4349ba983e
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-16 21:25:29 +0800

    added notification dot on cart icon

--- assets/application.js
+++ assets/application.js
@@ -1,12 +1,1 @@
-const store = Vue.reactive({
-    state: {
-        cartState: []
-    },
-    getCart() {
-        axios.get('/cart.js').then(response => {
-            this.state.cartState.unshift(response.data)
-        }).catch(error => {
-            console.log(`Error on store fn: ${error}`)
-        })
-    }
-})
+
