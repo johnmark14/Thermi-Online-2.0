68e4036b8b467fae97c35f1b09286a64b8fd3919
johnmark14 <johmarkgabrentina@gmail.com>
2021-09-17 16:44:39 +0800

    added login functionality

--- assets/application.js
+++ assets/application.js
@@ -9,12 +9,12 @@
 //     }, 1300);
 // })
 
-window.addEventListener("load", function(event) {
-    const loader = document.getElementById('loading');
-    setTimeout(function() {
-        loader.style.opacity = '0';
-        setTimeout(function() {
-            loader.remove();
-        }, 500)
-    }, 900);
-  });
+// window.addEventListener("load", function(event) {
+//     const loader = document.getElementById('loading');
+//     setTimeout(function() {
+//         loader.style.opacity = '0';
+//         setTimeout(function() {
+//             loader.remove();
+//         }, 500)
+//     }, 900);
+//   });
